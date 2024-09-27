1f31a51750b6fc86ebc1cd2348cb54bc5f9134d2
Zulfiqar <8zali9@gmail.com>
2024-09-27 21:35:37 +0500

    Initial v1

---
+++ index.js
@@ -0,0 +1,46 @@
+const http = require("http")
+
+class ServerZ {
+    constructor() {
+        this.handlers = {}
+        this.server = http.createServer((req, res) => {
+            const {url, method} = req
+            const reqKey = `${method}: ${url}`
+
+            if (this.handlers[reqKey]) this.handlers[reqKey](req, res)
+            else {
+                res.setHeader("Content-Type", "application/json")
+                res.end(JSON.stringify({
+                    status: 400,
+                    error: "Handler not defined"
+                }))
+            }
+        })
+    }
+
+    getReq(url, handler) {
+        const reqKey = `GET: ${url}`
+        this.handlers[reqKey] = handler
+    }
+
+    postReq(url, handler) {
+        const reqKey = `POST: ${url}`
+        this.handlers[reqKey] = handler
+    }
+
+    updateReq(url, handler) {
+        const reqKey = `UPDATE: ${url}`
+        this.handlers[reqKey] = handler
+    }
+
+    deleteReq(url, handler) {
+        const reqKey = `DELETE: ${url}`
+        this.handlers[reqKey] = handler
+    }
+
+    listen(port, callback) {
+        this.server.listen(port, callback)
+    }
+}
+
+module.exports = ServerZ
